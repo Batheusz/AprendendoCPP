d9a089f6467cdb5f8ff8f00a94aae1f56d3fd96d
Matheus Henrique Orsini <matheus1506200@outlook.com>
2020-12-29 21:47:48 -0300

    Create Exercício3.cpp

---
+++ Capítulo2/Exercício3.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int main()
+{
+    //Exercício 3)A
+    int c{0}, thisIsAVariable{0}, q76354{0}, number{0};
+    std::cout << c << thisIsAVariable << q76354 << number;
+    //Exercício 3)B
+    int numero;
+    std::cout << "\n\nDigite um numero inteiro: ";
+    std::cin >> numero;
+    //Exercício 3)D
+    if (numero != 7)
+    {
+        std::cout << "A variavel numero nao eh igual a 7\n";
+    }
+    //Exercício 3)C
+    int idade;
+    std::cout << "Digite sua idade: ";
+    std::cin >> idade;
+    //Exercício 3)E
+    std::cout << "This is a C++ program\n";
+    //Exercício 3)F
+    std::cout << "This is a C++\nprogram" << std::endl;
+    //Exercício 3)G
+    std::cout << "This\nis\na\nC++\nprogram\n";
+    //Exercício 3)H
+    std::cout << "This\tis\ta\tC++\tprogram\n";
+    
+}
