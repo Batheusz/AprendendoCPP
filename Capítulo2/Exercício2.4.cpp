f9a7646db7219fe5266b9ef211fd00a42d266b8c
Matheus Henrique Orsini <matheus1506200@outlook.com>
2020-12-30 15:53:32 -0300

    Update Exercício2.4.cpp

--- Capítulo2/Exercício2.4.cpp
+++ Capítulo2/Exercício2.4.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    página 267
+    //Exercício 2.4)A
+    int num1, num2, num3;
+    cout << "Digite seus tres numeros: ";
+    cin >> num1 >> num2 >> num3;
+    cout << num1 * num2 * num3;
+    //Exercício 2.4)B
+    int x {0};
+    int y {0};
+    int z {0};
+    int resultado {0};
+    //Exercício 2.4)D
+    cout << "Digite tres numeros: ";
+    cin >> x >> y >> z;
+    //Exercício 2.4)E
+    resultado = x * y * z;
+    //Exercício 2.4)F
+    cout << "O produto eh ";
+    cin >> resultado;
+    
     return 0;
 }
