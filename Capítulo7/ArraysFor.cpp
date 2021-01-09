6be3cf76e1c0975cf97e300391a3405242dc0e60
Matheus Henrique Orsini <matheus1506200@outlook.com>
2021-01-09 11:20:45 -0300

    Create ArraysFor.cpp

---
+++ Capítulo7/ArraysFor.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <array>
+using namespace std;
+
+int main()
+{
+    //Existe uma outra forma de mostrar os elementos de um array usando de for.
+    //A que foi utilizada anteriormente era com a declaração padrão de um for. for(contador; condição; incremento)
+    array<int, 10> numeros{1,2,3,4,5,6,7,8,9,10};
+    for( int i : numeros)
+    //for(variável do mesmo tipo do array/lista de elementos : arrays/lista de elementos).
+    //A variável irá percorrer toda a lista e executar os comandos do bloco.
+    {
+        cout << i << " ";
+    }
+    return 0;
+}
